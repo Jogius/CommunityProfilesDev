4d62dceff65582a8673ec823359901bd1109a917
Julius Makowski <jogius.julius.makowski@gmail.com>
2022-03-03 21:39:37 +0100

    asynccc

--- .github/workflows/ProfileRepoSummarizeAction/index.js
+++ .github/workflows/ProfileRepoSummarizeAction/index.js
@@ -11,36 +11,38 @@ class CommunityProfile {
   }
 }
 
-try {
-  const githubToken = core.getInput('token');
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload:\n\n${payload}`);
-
-  const client = github.getOctokit(githubToken);
-  let org = github.context.repo.owner;
-  let repoName = github.context.repo.repo;
-
-  const profiles = await client.rest.repos.getContent(org, repoName, "CustomProfiles");
-
-	console.log(`client.rest.repos.getContent() returned:\n\n${JSON.stringify(profiles, undefined, 2)}`);
-
-  // // loop through available profiles
-  // for (let index = 0; index < profiles.data.length; index++) {
-  //   let profile = profiles.data.at(index)
-
-  //   if (profile.type != "dir")
-  //     continue;
-
-  //   let name = profile.name
-  // }
-
-  // for (const profile in profiles.data) {
-  //   if (Object.hasOwnProperty.call(profiles.data, profiles)) {
-  //     const element = profiles.data[profiles];
-  //     console.log("a");
-  //   }
-  // }
-} catch (error) {
-	core.setFailed(error.message);
-}
+(async () => {
+  try {
+    const githubToken = core.getInput('token');
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload:\n\n${payload}`);
+  
+    const client = github.getOctokit(githubToken);
+    let org = github.context.repo.owner;
+    let repoName = github.context.repo.repo;
+  
+    const profiles = await client.rest.repos.getContent(org, repoName, "CustomProfiles");
+  
+    console.log(`client.rest.repos.getContent() returned:\n\n${JSON.stringify(profiles, undefined, 2)}`);
+  
+    // // loop through available profiles
+    // for (let index = 0; index < profiles.data.length; index++) {
+    //   let profile = profiles.data.at(index)
+  
+    //   if (profile.type != "dir")
+    //     continue;
+  
+    //   let name = profile.name
+    // }
+  
+    // for (const profile in profiles.data) {
+    //   if (Object.hasOwnProperty.call(profiles.data, profiles)) {
+    //     const element = profiles.data[profiles];
+    //     console.log("a");
+    //   }
+    // }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
--- .github/workflows/TestJS/index.js
+++
@@ -1,1 +0,0 @@
-console.log("action has been run")
