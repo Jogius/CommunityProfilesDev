4f578fe47ae2a4d66de1897c03d61235c48c659c
Julius Makowski <jogius.julius.makowski@gmail.com>
2022-03-03 22:17:00 +0100

    progress? :c

--- .github/workflows/ProfileRepoSummarizeAction/index.js
+++ .github/workflows/ProfileRepoSummarizeAction/index.js
@@ -2,12 +2,11 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 class CommunityProfile {
-  constructor(name, description, creators, imageUrl, isInstalled) {
+  constructor(name, description, creators, imageUrl) {
     this.name = name;
     this.description = description;
     this.creators = creators;
     this.imageUrl = imageUrl;
-    this.isInstalled = isInstalled;
   }
 }
 
@@ -15,42 +14,30 @@ class CommunityProfile {
   try {
     const githubToken = process.env['token'];
 
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const context = JSON.stringify(github.context, undefined, 2);
-    console.log(`The context:\n\n${context}`);
-  
     const client = github.getOctokit(githubToken);
-    console.log('created client (octokit)');
 
     let owner = github.context.payload.repository.owner.name;
-    console.log(`org: ${owner}`);
     let repo = github.context.payload.repository.name;
-    console.log(`repoName: ${repo}`);
   
-    const profiles = await client.rest.repos.getContent({
+    const customProfilesRepoContent = await client.rest.repos.getContent({
       owner,
       repo,
       path: "CustomProfiles",
     });
   
-    console.log(`client.rest.repos.getContent() returned:\n\n${JSON.stringify(profiles, undefined, 2)}`);
-  
-    // // loop through available profiles
-    // for (let index = 0; index < profiles.data.length; index++) {
-    //   let profile = profiles.data.at(index)
-  
-    //   if (profile.type != "dir")
-    //     continue;
-  
-    //   let name = profile.name
-    // }
-  
-    // for (const profile in profiles.data) {
-    //   if (Object.hasOwnProperty.call(profiles.data, profiles)) {
-    //     const element = profiles.data[profiles];
-    //     console.log("a");
-    //   }
-    // }
+    // loop through available profiles
+    customProfilesRepoContent.data.forEach((el) => {
+      if (el.type != 'dir') continue;
+
+      const profileContent = await client.rest.repos.getContent({
+        owner,
+        repo,
+        path: el.path,
+      });
+      console.log(`profile ${el.path} data:\n\n${JSON.stringify(profileContent.data, undefined, 2)}`);
+
+    });
+
   } catch (error) {
     core.setFailed(error.message);
   }
